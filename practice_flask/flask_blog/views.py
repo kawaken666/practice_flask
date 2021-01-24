5d537d39626772415ace2a66658dfe13941f52d5
kawaken666 <kawaken2.666@gmail.com>
2021-01-24 20:39:20 +0900

    ・htmlの重複部分をレイアウトに集約 ・セッションを実装

--- practice_flask/flask_blog/views.py
+++ practice_flask/flask_blog/views.py
@@ -3,19 +3,27 @@ from flask_blog import app
 
 @app.route('/')
 def show_entries():
+    # セッションでlogged_inがTrueでない場合はログイン画面にリダイレクトしている
+    if not session.get('logged_in'):
+        return redirect('/login')
     return render_template('entries/index.html')
 
 @app.route('/login', methods={'GET', 'POST'})
 def login():
+    error = None
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
             print('ユーザ名が異なります')
         elif request.form['password'] != app.config['PASSWORD']:
             print('パスワードが異なります')
         else:
+            # セッションにlogged_in=Trueを渡す
+            session['logged_in'] = True
             return redirect('/')
     return render_template('login.html')
 
 @app.route('/logout')
 def logout():
+    # ログアウト時、セッションのlogged_inをNoneに更新する
+    session.pop('logged_in', None)
     return redirect('/')
