7de5aa28e5c13dd8cfeeac0c76cae3df7fd891a2
kawaken666 <kawaken2.666@gmail.com>
2021-01-25 04:43:42 +0900

    ・詳細表示機能を実装

--- practice_flask/flask_blog/models/entries.py
+++ practice_flask/flask_blog/models/entries.py
@@ -6,11 +6,14 @@ class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), unique=True)
     text = db.Column(db.Text)
-    created_at = db.Column(db.DateTime)
+    # なぜかコンストラクタでcreated_atに日時を格納できないので応急処置で以下デフォルトを設けている
+    # ビューの呼び出し側、またはINSERT処理に問題ある可能性もあり
+    created_at = db.Column(db.DateTime, default=datetime.utcnow())
 
 def __init__(self, title=None, text=None):
     self.title = title
     self.text = text
+    # 下記でdatetimeが入れれてない、もしくは、呼び出し側orINSERT処理に問題ある可能性あり
     self.created_at = datetime.utcnow()
 
 def __repr__(self):
