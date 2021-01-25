5dd57513e6863a47bcd29e2c2c23c061bb8006d9
kawaken666 <kawaken2.666@gmail.com>
2021-01-25 19:41:00 +0900

    実装完了

--- practice_flask/flask_blog/scripts/db.py
+++ practice_flask/flask_blog/scripts/db.py
@@ -1,9 +1,19 @@
+# DB操作のスクリプトファイル
+
 from flask_script import Command
 from flask_blog import db
 
+# 作成したモデルをDBに反映するコマンド
 class InitDB(Command):
     # 下記はスクリプト説明のコメント
     "create database"
 
     def run(self):
         db.create_all()
+
+# テスト完了後用のDB削除コマンド
+class DropDB(Command):
+    "drop database"
+
+    def run(self):
+        db.drop_all()
--- practice_flask/manage.py
+++ practice_flask/manage.py
@@ -3,9 +3,12 @@
 from flask_script import Manager
 from flask_blog import create_app
 
-from flask_blog.scripts.db import InitDB
+from flask_blog.scripts.db import InitDB, DropDB
 
 if __name__ == "__main__":
     manager = Manager(create_app)
+    # init_dbコマンドを実行できるようにする
     manager.add_command('init_db', InitDB())
+    # drop_dbコマンドを実行できるようにする
+    manager.add_command('drop_db', DropDB())
     manager.run()
